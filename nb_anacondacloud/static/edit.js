9bb1e000893c220fb2bba7b76d15a16f4fb8ed95
Marcos Vanetta <marcosvanetta@gmail.com>
2015-12-16 22:45:38 -0600

    tree and edit plugins no longer needed

--- nb_anacondacloud/static/edit.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function ($) {
-    var load_ipython_extension = function () {
-        console.log("edit");
-    };
-
-    return {
-        load_ipython_extension : load_ipython_extension,
-    };
-});
--- nb_anacondacloud/static/tree.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function ($) {
-    var load_ipython_extension = function () {
-        console.log("tree");
-    };
-
-    return {
-        load_ipython_extension : load_ipython_extension,
-    };
-});
