61a17c3cdac7fd1601b44a9d48f094a4cb282dfe
Nicholas Bollweg <nbollweg@continuum.io>
2016-01-15 10:57:51 -0500

    updating/removing  patch for notebook 4.1, adding first test

--- nb_anacondacloud/tests/js/test_basics.js
+++ nb_anacondacloud/tests/js/test_basics.js
@@ -1,5 +1,15 @@
 var t = casper.test;
 
+
+function basic_test(){
+
+  this.baseline_notebook();
+
+  this.canSeeAndClick("the nb_anacondacloud button", "#publish_notebook");
+  this.canSeeAndClick("the modal cancel button", ".modal-dialog .btn[data-dismiss=modal]")
+}
+
 casper.notebook_test(function(){
-  t.assert(true, "yup, it's true!");
+  casper.viewport(1440, 900)
+    .then(basic_test);
 });
--- nb_anacondacloud/tests/test_notebook.py
+++ nb_anacondacloud/tests/test_notebook.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import glob
 
 try:
@@ -8,10 +7,7 @@ except ImportError:
     # py2
     from mock import patch
 
-import requests
-
 from notebook import jstest
-from ipython_genutils.tempdir import TemporaryDirectory
 
 here = os.path.dirname(__file__)
 
@@ -48,63 +44,6 @@ class NBAnacondaCloudTestController(jstest.JSController):
 
         if extra_args is not None:
             self.cmd = self.cmd + extra_args
-
-    def setup(self):
-        # call the hacked setup
-        self._setup()
-
-        # patch
-        # with patch.dict(os.environ, self.env):
-        #     install_kwargs = dict(enable=True, user=True)
-        #     if "CONDA_ENV_PATH" in os.environ:
-        #         install_kwargs.pop("user")
-        #         install_kwargs.update(prefix=os.environ["CONDA_ENV_PATH"])
-        #     install(**install_kwargs)
-
-    def _setup(self):
-        """ copy pasta from
-            https://github.com/jupyter/notebook/blob/4.0.6/notebook/jstest.py
-            master has some changes that will ship with 4.1...
-        """
-        self.ipydir = TemporaryDirectory()
-        self.config_dir = TemporaryDirectory()
-        self.nbdir = TemporaryDirectory()
-        self.home = TemporaryDirectory()
-        self.env = {
-            'HOME': self.home.name,
-            'JUPYTER_CONFIG_DIR': self.config_dir.name,
-            'IPYTHONDIR': self.ipydir.name,
-        }
-        self.dirs.append(self.ipydir)
-        self.dirs.append(self.home)
-        self.dirs.append(self.config_dir)
-        self.dirs.append(self.nbdir)
-        os.makedirs(os.path.join(self.nbdir.name, 'sub dir1', 'sub dir 1a'))
-        os.makedirs(os.path.join(self.nbdir.name, 'sub dir2', 'sub dir 1b'))
-
-        if self.xunit:
-            self.add_xunit()
-
-        # If a url was specified, use that for the testing.
-        if self.url:
-            try:
-                alive = requests.get(self.url).status_code == 200
-            except:
-                alive = False
-
-            if alive:
-                self.cmd.append("--url=%s" % self.url)
-            else:
-                raise Exception('Could not reach "%s".' % self.url)
-        else:
-            # start the ipython notebook, so we get the port number
-            self.server_port = 0
-            self._init_server()
-            if self.server_port:
-                self.cmd.append("--port=%i" % self.server_port)
-            else:
-                # don't launch tests if the server didn't start
-                self.cmd = [sys.executable, '-c', 'raise SystemExit(1)']
 
 
 def prepare_controllers(options):
