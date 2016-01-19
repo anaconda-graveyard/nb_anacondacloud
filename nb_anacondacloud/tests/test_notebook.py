6d6a0f35de264b680cfadfacbc85cda99fe9bdae
Nicholas Bollweg <nbollweg@continuum.io>
2016-01-19 10:28:56 -0500

    restoring testcontroller.set because anaconda cloud unicode

--- nb_anacondacloud/tests/test_notebook.py
+++ nb_anacondacloud/tests/test_notebook.py
@@ -75,6 +75,7 @@ class NBAnacondaCloudTestController(jstest.JSController):
                 "--prefix", self.config_dir.name,
             ])
 
+        # THIS IS FROM jstest
         "Start the notebook server in a separate process"
         self.server_command = command = [
             sys.executable,
@@ -110,6 +111,51 @@ class NBAnacondaCloudTestController(jstest.JSController):
             'nbserver-%i.json' % self.server.pid
         )
         self._wait_for_server()
+
+    def setup(self):
+        # the fancy dir names confuse anaconda build
+        self.ipydir = jstest.TemporaryDirectory()
+        self.config_dir = jstest.TemporaryDirectory()
+        self.nbdir = jstest.TemporaryDirectory()
+        self.home = jstest.TemporaryDirectory()
+        self.env = {
+            'HOME': self.home.name,
+            'JUPYTER_CONFIG_DIR': self.config_dir.name,
+            'IPYTHONDIR': self.ipydir.name,
+        }
+        self.dirs.append(self.ipydir)
+        self.dirs.append(self.home)
+        self.dirs.append(self.config_dir)
+        self.dirs.append(self.nbdir)
+        os.makedirs(os.path.join(
+            self.nbdir.name, os.path.join(u'sub dir1', u'sub dir 1a')))
+        os.makedirs(os.path.join(
+            self.nbdir.name, os.path.join(u'sub dir2', u'sub dir 1b')))
+
+        if self.xunit:
+            self.add_xunit()
+
+        # If a url was specified, use that for the testing.
+        if self.url:
+            try:
+                alive = jstest.requests.get(self.url).status_code == 200
+            except:
+                alive = False
+
+            if alive:
+                self.cmd.append("--url=%s" % self.url)
+            else:
+                raise Exception('Could not reach "%s".' % self.url)
+        else:
+            # start the ipython notebook, so we get the port number
+            self.server_port = 0
+            self._init_server()
+            if self.server_port:
+                self.cmd.append('--url=http://localhost:%i%s' % (
+                    self.server_port, self.base_url))
+            else:
+                # don't launch tests if the server didn't start
+                self.cmd = [sys.executable, '-c', 'raise SystemExit(1)']
 
 
 def prepare_controllers(options):
