0d0f0e17e8d8bc10237417342c54d4031af8fbfc
Nicholas Bollweg <nbollweg@continuum.io>
2016-02-01 11:39:19 -0500

    updating tests to match new UI, remove time-based steps (add back later with mock delays?

--- nb_anacondacloud/tests/js/auth/test_01_auth.js
+++ nb_anacondacloud/tests/js/auth/test_01_auth.js
@@ -6,26 +6,21 @@ function basic_test(){
 
   this.baseline_notebook();
 
-  //this.runCell(0, ["!anaconda whoami"]);
-
-  this.canSeeAndClick("the nbac button", "#publish_notebook")
+  this.canSeeAndClick("the nbac toolbar button", "#publish_notebook")
     .then(function(){ return this.wait(300); })
     .canSeeAndClick("the upload header", "h4.modal-title")
     .then(function(){
-      t.assertSelectorHasText("h4.modal-title", "Upload");
+      t.assertSelectorHasText("h4.modal-title", "Publish");
     })
-    .canSeeAndClick("the ok button", ".modal-footer .btn-primary")
-    .waitForText("Uploading")
-    .canSeeAndClick("the uploading notification", "#notification_notebook")
-    .waitForText("Your notebook has been uploaded")
-    .canSeeAndClick("the success notification", "#notification_notebook")
+    .canSeeAndClick("the publish button", ".modal-body .btn-success")
+    .canSeeAndClick("the visit toolbar button", "#visit_notebook")
     .then(function(){ return this.wait(3000); })
     .canSeeAndClick("fin", "#publish_notebook");
 }
 
 
 casper.notebook_test(function(){
-  casper.screenshot.init("01_auth");
+  casper.screenshot.init("auth_create");
   casper.viewport(1440, 900)
     .then(basic_test)
 });
