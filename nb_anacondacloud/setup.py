bba4f81cee7ded7f0a7ae4595a8c7f836cf1c582
Marcos Vanetta <marcosvanetta@gmail.com>
2016-03-10 12:12:01 -0700

    Remove old client extensions installer

--- nb_anacondacloud/setup.py
+++
@@ -1,9 +0,0 @@
-from os.path import join, abspath, dirname
-from nbsetuptools import setup
-
-
-setup(
-    name="nb_anacondacloud",
-    version="0.1.1",
-    static=join(abspath(dirname(__file__)), 'static')
-)
