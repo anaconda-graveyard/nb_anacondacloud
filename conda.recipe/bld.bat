96d068003759f1b5d6c0674c792813afbc687486
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-11 11:26:53 -0500

    ah, an if approaches

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,1 +1,1 @@
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py install && if errorlevel 1 exit 1
--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,3 +1,1 @@
-"%PREFIX%\Scripts\npm.cmd" install
-"%PREFIX%\Scripts\npm.cmd" run test
-if errorlevel 1 exit 1
+"%PREFIX%\Scripts\npm.cmd" install && "%PREFIX%\Scripts\npm.cmd" run test && if errorlevel 1 exit 1
