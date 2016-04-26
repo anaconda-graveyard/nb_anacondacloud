35c9787c7e812a232aaff079e84b9b175ca8db3d
Nicholas Bollweg <nick.bollweg@gmail.com>
2016-04-26 10:53:53 -0400

    Heard you like envs in your envs

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\node_modules\npm\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\node_modules\npm\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
+"%PREFIX%\node_modules\npm\bin\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\node_modules\npm\bin\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
