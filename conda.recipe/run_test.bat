c187aa98ca09c2e0c2866fe7e631096e80f9d77f
Nicholas Bollweg <nick.bollweg@gmail.com>
2016-04-26 10:14:14 -0400

    Use most canonical location for npm

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\npm.cmd" install -g npm@latest --no-spin && "%PREFIX%\Scripts\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\Scripts\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
+"%PREFIX%\node_modules\npm\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\node_modules\npm\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
